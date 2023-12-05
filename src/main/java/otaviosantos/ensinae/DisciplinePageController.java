c98793781f0193fa0b09855f1d3e233806015a6b
oivatO-soD-sotnaS <116353611+oivatO-soD-sotnaS@users.noreply.github.com>
2023-12-05 10:14:46 -0300

    utd

--- src/main/java/otaviosantos/ensinae/DisciplinePageController.java
+++ src/main/java/otaviosantos/ensinae/DisciplinePageController.java
@@ -1,4 +1,61 @@
 package otaviosantos.ensinae;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import org.w3c.dom.events.MouseEvent;
+
 public class DisciplinePageController {
+    @FXML
+    private TableColumn<?, ?> cpf;
+
+    @FXML
+    private TableColumn<?, ?> email;
+
+    @FXML
+    private TableColumn<?, ?> name;
+
+    @FXML
+    private Button removeStudantButton;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Label studantCPF;
+
+    @FXML
+    private Label studantEmail;
+
+    @FXML
+    private Label studantName;
+
+    @FXML
+    private TableView<?> studantsTableView;
+
+    @FXML
+    private TableColumn<?, ?> surname;
+
+    @FXML
+    void getStudantInfo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void removeStudant(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToHomePage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateTable(ActionEvent event) {
+
+    }
+
+    public void getStudantInfo(javafx.scene.input.MouseEvent mouseEvent) {
+    }
 }
